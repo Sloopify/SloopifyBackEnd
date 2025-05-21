d6cb2de05ee0c4c9846bd07b669b3ddfdc0e0ce9
Ebrahem Alwish <ebrahemalwish@MacBook-Air-Ebrahem.local>
2025-05-21 14:11:35 +0300

    user mode guard

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\AdminAuthMiddleware;
+use App\Http\Middleware\UserAuthMiddleware;
+use Illuminate\Support\Facades\Route;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -14,11 +17,17 @@ return Application::configure(basePath: dirname(__DIR__))
                 ->prefix('admin')
                 ->name('admin.')
                 ->group(base_path('routes/Admin.php'));
+
+            Route::middleware('web')
+                ->prefix('user')
+                ->name('user.')
+                ->group(base_path('routes/User.php'));
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'admin.auth' => AdminAuthMiddleware::class
+            'admin.auth' => AdminAuthMiddleware::class,
+            'user.auth' => UserAuthMiddleware::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,27 +13,30 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('gender', ['male', 'female'])->default('male')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->boolean('is_blocked')->default(false);
+            $table->integer('age')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('phone');
+            $table->string('img')->nullable();
+            $table->string('bio')->nullable();
+            $table->string('referral_code')->nullable();
+            $table->string('referral_link')->nullable();
+            $table->string('reffred_by')->nullable();
+            $table->date('last_login_at')->nullable();
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->text('google_id')->nullable();
+            $table->enum('provider',['google' , 'apple'])->nullable();
+            $table->text('device_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
         });
     }
 
