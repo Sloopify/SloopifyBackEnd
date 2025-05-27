aa167e68a10fc3450f9a60cace604ad428ddb747
Ebrahem Alwish <ebrahemalwish@MacBook-Air-Ebrahem.local>
2025-05-27 12:31:06 +0300

    edit user table gender

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('gender', ['male', 'female'])->default('male')->nullable();
+            $table->enum('gender', ['male', 'female'])->nullable();
             $table->enum('status', ['active', 'inactive'])->default('active');
             $table->boolean('is_blocked')->default(false);
             $table->integer('age')->nullable();
